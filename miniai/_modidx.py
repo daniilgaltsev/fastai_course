ccbe6bd90563a520a1c898461ccd380266a2ce3c
Daniil Galtsev <galtsev.daniil@gmail.com>
2023-11-19 15:54:37 +0000

    replicate 05_datasets from lesson 14

--- miniai/_modidx.py
+++ miniai/_modidx.py
@@ -5,4 +5,10 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://daniilgaltsev.github.io',
                 'git_url': 'https://github.com/daniilgaltsev/fastai_course',
                 'lib_path': 'miniai'},
-  'syms': {'miniai.core': {'miniai.core.foo': ('core.html#foo', 'miniai/core.py')}}}
+  'syms': { 'miniai.core': {'miniai.core.foo': ('core.html#foo', 'miniai/core.py')},
+            'miniai.datasets': { 'miniai.datasets.collate_dict': ('datasets.html#collate_dict', 'miniai/datasets.py'),
+                                 'miniai.datasets.get_grid': ('datasets.html#get_grid', 'miniai/datasets.py'),
+                                 'miniai.datasets.inplace': ('datasets.html#inplace', 'miniai/datasets.py'),
+                                 'miniai.datasets.show_image': ('datasets.html#show_image', 'miniai/datasets.py'),
+                                 'miniai.datasets.show_images': ('datasets.html#show_images', 'miniai/datasets.py'),
+                                 'miniai.datasets.subplots': ('datasets.html#subplots', 'miniai/datasets.py')}}}
